eba4797e8edc048a14f2ddb7c8a0c3cf327ad72a
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2025-05-08 14:15:09 -0400

    Update lspconfig.lua
    add vue lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -177,6 +177,7 @@ return {
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       html = {},
       htmx = {},
+      vuels = {},
       ts_ls = {},
       clangd = {},
       tailwindcss = {},
