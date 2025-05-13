2fbde1a4e505f33fa88f37474a6d51ca8a61a21c
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2025-05-13 17:13:48 -0400

    changed harpoon and lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -177,14 +177,14 @@ return {
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       html = {},
       ts_ls = {
-        init_options = {
-          plugins = {
-            name = '@vue/typescript-plugin',
-            location = '/usr/local/lib/node_modules/@vue/language-server',
-            languages = { 'vue' },
-          },
-        },
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        -- init_options = {
+        --   plugins = {
+        --     name = '@vue/typescript-plugin',
+        --     location = '/usr/local/lib/node_modules/@vue/language-server',
+        --     languages = { 'vue' },
+        --   },
+        -- },
+        -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
       },
       clangd = {},
       tailwindcss = {},
