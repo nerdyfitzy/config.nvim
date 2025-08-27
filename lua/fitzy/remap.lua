09925f7d13c5d44869908cbc3eea051262e77a91
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2025-08-27 14:48:15 -0400

    Update remap.lua

--- lua/fitzy/remap.lua
+++ lua/fitzy/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = false })
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
